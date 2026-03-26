b72c8f6f9c9444c1ab7be50c73fe81b4a3098711
joshua <joshua.mbilli@student.moringaschool.com>
2026-03-26 19:19:18 +0300

    Update for index.js

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 //Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
 
+function logAttendeeName() {
+  console.log(attendee.name);
+}
+
+function logTicketPrice() {
+  console.log(attendee.ticketPrice);
+}
+
+function updateTicketType(attendee, newticketType) {
+  attendee.ticketType = newticketType;
+  console.log(attendee);
+}
+
+function updateTicketPrice(attendee, newticketPrice) {
+  attendee.ticketPrice = newticketPrice;
+  console.log(attendee);
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+  console.log(attendee);
+}
+
+function addCheckedInProperty(attendee) {
+    attendee.checkedIn = true;
+    console.log(attendee);
+}
 
 
 //Needed for the tests to work. Don't modify
